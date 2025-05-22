543faa31c3cc95c2f609a1ad83f0e71da29594c1
slgao <g13827276024@163.com>
2025-05-22 16:15:43 +0800

    add blip2 UT

--- tests/transformers_tests/models/blip_2/test_modeling_blip_2.py
+++ tests/transformers_tests/models/blip_2/test_modeling_blip_2.py
@@ -9,21 +9,17 @@
 # it is necessary to develop distinct, dedicated test cases.
 
 import inspect
+import unittest
 
 import numpy as np
 import pytest
 import torch
+from parameterized import parameterized
 from transformers import Blip2VisionConfig
 
 import mindspore as ms
 
-from tests.modeling_test_utils import (
-    MS_DTYPE_MAPPING,
-    PT_DTYPE_MAPPING,
-    compute_diffs,
-    generalized_parse_args,
-    get_modules,
-)
+from tests.modeling_test_utils import forward_compare
 from tests.transformers_tests.models.modeling_common import floats_numpy
 
 # fp16 got nan
@@ -90,95 +86,31 @@ class Blip2VisionModelTester:
         )
 
 
-blip2_text_config, pixel_values = Blip2VisionModelTester().prepare_config_and_inputs()
+class Blip2VisionModelTest(unittest.TestCase):
+    def setUp(self):
+        self.model_tester = Blip2VisionModelTester()
 
-
-BLIP2_CASES = [
-    [
-        "Blip2VisionModel",
-        "transformers.Blip2VisionModel",
-        "mindone.transformers.Blip2VisionModel",
-        (blip2_text_config,),
-        {},
-        (pixel_values,),
-        {},
-        {
-            "last_hidden_state": 0,
-            "pooler_output": 1,
-        },
-    ],
-]
-
-
-@pytest.mark.parametrize(
-    "name,pt_module,ms_module,init_args,init_kwargs,inputs_args,inputs_kwargs,outputs_map,dtype,mode",
-    [
-        case
-        + [
-            dtype,
-        ]
-        + [
-            mode,
-        ]
-        for case in BLIP2_CASES
-        for dtype in DTYPE_AND_THRESHOLDS.keys()
-        for mode in MODES
-    ],
-)
-def test_named_modules(
-    name,
-    pt_module,
-    ms_module,
-    init_args,
-    init_kwargs,
-    inputs_args,
-    inputs_kwargs,
-    outputs_map,
-    dtype,
-    mode,
-):
-    ms.set_context(mode=mode)
-
-    (
-        pt_model,
-        ms_model,
-        pt_dtype,
-        ms_dtype,
-    ) = get_modules(pt_module, ms_module, dtype, *init_args, **init_kwargs)
-    pt_inputs_args, pt_inputs_kwargs, ms_inputs_args, ms_inputs_kwargs = generalized_parse_args(
-        pt_dtype, ms_dtype, *inputs_args, **inputs_kwargs
+    @parameterized.expand(
+        [[dtype,] + [mode,] for dtype in DTYPE_AND_THRESHOLDS for mode in MODES]
     )
+    def test_model_forward(self, dtype, mode):
+        ms.set_context(mode=mode)
+        pt_module = "transformers.Blip2VisionModel"
+        ms_module = "mindone.transformers.Blip2VisionModel"
+        blip2_text_config, pixel_values = self.model_tester.prepare_config_and_inputs()
+        init_args = (blip2_text_config,)
+        init_kwargs = {}
+        inputs_args = (pixel_values,)
+        inputs_kwargs = {}
+        outputs_map = {"last_hidden_state": 0, "pooler_output": 1}
 
-    # set `hidden_dtype` if requiring, for some modules always compute in float
-    # precision and require specific `hidden_dtype` to cast before return
-    if "hidden_dtype" in inspect.signature(pt_model.forward).parameters:
-        pt_inputs_kwargs.update({"hidden_dtype": PT_DTYPE_MAPPING[pt_dtype]})
-        ms_inputs_kwargs.update({"hidden_dtype": MS_DTYPE_MAPPING[ms_dtype]})
+        diffs, pt_dtype, ms_dtype = forward_compare(
+            pt_module, ms_module, init_args, init_kwargs, inputs_args, inputs_kwargs, outputs_map, dtype
+        )
 
-    with torch.no_grad():
-        pt_outputs = pt_model(*pt_inputs_args, **pt_inputs_kwargs)
-    ms_outputs = ms_model(*ms_inputs_args, **ms_inputs_kwargs)
-    # print("ms:", ms_outputs)
-    # print("pt:", pt_outputs)
-    if outputs_map:
-        pt_outputs_n = []
-        ms_outputs_n = []
-        for pt_key, ms_idx in outputs_map.items():
-            # print("===map", pt_key, ms_idx)
-            pt_output = getattr(pt_outputs, pt_key)
-            ms_output = ms_outputs[ms_idx]
-            if isinstance(pt_output, (list, tuple)):
-                pt_outputs_n += list(pt_output)
-                ms_outputs_n += list(ms_output)
-            else:
-                pt_outputs_n.append(pt_output)
-                ms_outputs_n.append(ms_output)
-        diffs = compute_diffs(pt_outputs_n, ms_outputs_n)
-    else:
-        diffs = compute_diffs(pt_outputs, ms_outputs)
-
-    THRESHOLD = DTYPE_AND_THRESHOLDS[ms_dtype]
-    assert (np.array(diffs) < THRESHOLD).all(), (
-        f"ms_dtype: {ms_dtype}, pt_type:{pt_dtype}, "
-        f"Outputs({np.array(diffs).tolist()}) has diff bigger than {THRESHOLD}"
-    )
+        THRESHOLD = DTYPE_AND_THRESHOLDS[ms_dtype]
+        self.assertTrue(
+            (np.array(diffs) < THRESHOLD).all(),
+            f"For Blip2VisionModel forward test, mode: {mode}, ms_dtype: {ms_dtype}, pt_type:{pt_dtype},"
+            f"Outputs({np.array(diffs).tolist()}) has diff bigger than {THRESHOLD}"
+        )
