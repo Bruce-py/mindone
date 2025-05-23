8b8734ed3396a0a7c383b0505afd521271ae6a8b
slgao <g13827276024@163.com>
2025-05-23 16:02:09 +0800

    add DPT UT

--- tests/transformers_tests/models/dpt/test_modeling_dpt.py
+++ tests/transformers_tests/models/dpt/test_modeling_dpt.py
@@ -8,22 +8,18 @@
 # In cases where models have unique initialization procedures or require testing with specialized output formats,
 # it is necessary to develop distinct, dedicated test cases.
 
-import inspect
+import unittest
 
 import numpy as np
 import pytest
-import torch
-from transformers import DPTConfig
+from parameterized import parameterized
+from transformers import DPTConfig, DPTImageProcessor
 
 import mindspore as ms
+from transformers.testing_utils import slow
 
-from tests.modeling_test_utils import (
-    MS_DTYPE_MAPPING,
-    PT_DTYPE_MAPPING,
-    compute_diffs,
-    generalized_parse_args,
-    get_modules,
-)
+from mindone.transformers import DPTForDepthEstimation
+from tests.modeling_test_utils import forward_compare, prepare_img
 from tests.transformers_tests.models.modeling_common import floats_numpy, ids_numpy
 
 # fp16 diff too large, resize not support bf16
@@ -109,96 +105,58 @@ class DPTModelTester:
         )
 
 
-model_tester = DPTModelTester()
-config, pixel_values, labels = model_tester.prepare_config_and_inputs()
+class DPTModelTest(unittest.TestCase):
+    def setUp(self):
+        self.model_tester = DPTModelTester()
 
-
-DPT_CASES = [
-    [
-        "DPTForDepthEstimation",
-        "transformers.DPTForDepthEstimation",
-        "mindone.transformers.DPTForDepthEstimation",
-        (config,),
-        {},
-        (pixel_values,),
-        {},
-        {
-            "predicted_depth": 0,
-        },
-    ],
-]
-
-
-# fp16 diff too large
-@pytest.mark.parametrize(
-    "name,pt_module,ms_module,init_args,init_kwargs,inputs_args,inputs_kwargs,outputs_map,dtype,mode",
-    [
-        case
-        + [
-            dtype,
-        ]
-        + [
-            mode,
-        ]
-        for case in DPT_CASES
-        for dtype in DTYPE_AND_THRESHOLDS.keys()
-        for mode in MODES
-    ],
-)
-def test_named_modules(
-    name,
-    pt_module,
-    ms_module,
-    init_args,
-    init_kwargs,
-    inputs_args,
-    inputs_kwargs,
-    outputs_map,
-    dtype,
-    mode,
-):
-    ms.set_context(mode=mode)
-
-    (
-        pt_model,
-        ms_model,
-        pt_dtype,
-        ms_dtype,
-    ) = get_modules(pt_module, ms_module, dtype, *init_args, **init_kwargs)
-    pt_inputs_args, pt_inputs_kwargs, ms_inputs_args, ms_inputs_kwargs = generalized_parse_args(
-        pt_dtype, ms_dtype, *inputs_args, **inputs_kwargs
+    @parameterized.expand(
+        [[dtype,] + [mode,] for dtype in DTYPE_AND_THRESHOLDS for mode in MODES]
     )
+    def test_model_forward(self, dtype, mode):
+        ms.set_context(mode=mode)
+        pt_module = "transformers.DPTForDepthEstimation"
+        ms_module = "mindone.transformers.DPTForDepthEstimation"
+        config, pixel_values, _ = self.model_tester.prepare_config_and_inputs()
+        init_args = (config,)
+        init_kwargs = {}
+        inputs_args = (pixel_values,)
+        inputs_kwargs = {}
+        outputs_map = {"predicted_depth": 0}
 
-    # set `hidden_dtype` if requiring, for some modules always compute in float
-    # precision and require specific `hidden_dtype` to cast before return
-    if "hidden_dtype" in inspect.signature(pt_model.forward).parameters:
-        pt_inputs_kwargs.update({"hidden_dtype": PT_DTYPE_MAPPING[pt_dtype]})
-        ms_inputs_kwargs.update({"hidden_dtype": MS_DTYPE_MAPPING[ms_dtype]})
+        diffs, pt_dtype, ms_dtype = forward_compare(
+            pt_module, ms_module, init_args, init_kwargs, inputs_args, inputs_kwargs, outputs_map, dtype
+        )
 
-    with torch.no_grad():
-        pt_outputs = pt_model(*pt_inputs_args, **pt_inputs_kwargs)
-    ms_outputs = ms_model(*ms_inputs_args, **ms_inputs_kwargs)
-    # print("ms:", ms_outputs)
-    # print("pt:", pt_outputs)
-    if outputs_map:
-        pt_outputs_n = []
-        ms_outputs_n = []
-        for pt_key, ms_idx in outputs_map.items():
-            # print("===map", pt_key, ms_idx)
-            pt_output = getattr(pt_outputs, pt_key)
-            ms_output = ms_outputs[ms_idx]
-            if isinstance(pt_output, (list, tuple)):
-                pt_outputs_n += list(pt_output)
-                ms_outputs_n += list(ms_output)
-            else:
-                pt_outputs_n.append(pt_output)
-                ms_outputs_n.append(ms_output)
-        diffs = compute_diffs(pt_outputs_n, ms_outputs_n)
-    else:
-        diffs = compute_diffs(pt_outputs, ms_outputs)
+        THRESHOLD = DTYPE_AND_THRESHOLDS[ms_dtype]
+        self.assertTrue(
+            (np.array(diffs) < THRESHOLD).all(),
+            f"For DPTForDepthEstimation forward test, mode: {mode}, ms_dtype: {ms_dtype}, pt_type:{pt_dtype},"
+            f"Outputs({np.array(diffs).tolist()}) has diff bigger than {THRESHOLD}"
+        )
 
-    THRESHOLD = DTYPE_AND_THRESHOLDS[ms_dtype]
-    assert (np.array(diffs) < THRESHOLD).all(), (
-        f"ms_dtype: {ms_dtype}, pt_type:{pt_dtype}, "
-        f"Outputs({np.array(diffs).tolist()}) has diff bigger than {THRESHOLD}"
-    )
+
+class DPTModelIntegrationTest(unittest.TestCase):
+    @parameterized.expand(MODES)
+    @slow
+    def test_model_inference_depth_estimation(self, mode):
+        ms.set_context(mode=mode)
+        model_name = "Intel/dpt-large"
+        image_processor = DPTImageProcessor.from_pretrained()
+        model = DPTForDepthEstimation.from_pretrained(model_name)
+
+        image_url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        image = prepare_img(image_url)
+        inputs = image_processor(images=image, return_tensors="np")
+        inputs["pixel_values"] = ms.Tensor(inputs["pixel_values"])
+
+        outputs = model(**inputs)
+        predicted_depth = outputs[0]
+
+        # check the predicted depth
+        EXPECTED_SHAPE = (1, 384, 384)
+        self.assertEqual(predicted_depth.shape, EXPECTED_SHAPE)
+
+        EXPECTED_SLICE = ms.Tensor(
+            [[6.3199, 6.3629, 6.4148], [6.3850, 6.3615, 6.4166], [6.3519, 6.3176, 6.3575]]
+        )
+        np.testing.assert_allclose(predicted_depth[0, :3, :3], EXPECTED_SLICE, rtol=1e-4, atol=1e-4)
